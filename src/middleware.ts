a60ba4e53e38f744ce6de563769cb01eb82baa6a
rovarth <rovgarth@proton.me>
2025-10-20 16:38:06 +0000

    changed middleware URL redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,14 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
-  const protectedRoutes = ["/dashboard", "/"];
+  const protectedRoutes = ["/dashboard"];
 
-  if (
-    protectedRoutes.some((route) =>
-      request.nextUrl.pathname.startsWith(route),
-    ) &&
-    !token
-  ) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route),
+  );
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
