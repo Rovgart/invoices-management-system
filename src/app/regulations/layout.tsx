8b1582ab58832e7e3da2ae9ce38277591a3b00ab
rovarth <rovgarth@proton.me>
2025-10-28 16:57:43 +0000

    More SEO friendly HTML tags

---
+++ src/app/regulations/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+function layout({ children }: { children: ReactNode }) {
+  return (
+    <article className="min-h-screen bg-background py-12 px-4 sm:px-6 lg:px-8">
+      {children}
+    </article>
+  );
+}
+
+export default layout;
